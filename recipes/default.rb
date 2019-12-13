73a58e1686f5bb3fd3d6898cd05864b51e0f744f
Mahesh <maheshvishnu9@gmail.com>
2019-12-13 12:36:29 -0500

    add pem

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,16 +1,28 @@
 #
-# Cookbook:: tomcat
+# Cookbook Name:: httpd
 # Recipe:: default
 #
-# Copyright:: 2019, The Authors, All Rights Reserved.
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+
 package 'httpd' do
-    action :install
-  end
-  service 'httpd' do
-    action [:enable, :start]
-  end
-  file '/Users/maheshbbauthottempudi/Desktop/cookbooks/index.html' do
-    content 'Welcome to devops training'
-    action :create
-  end
-  
+  action :install
+end
+
+service 'httpd' do
+  action [ :enable, :start ]
+end
+
+cookbook_file "/var/www/html/index.html" do
+  source "index.html"
+  mode "0644"
+end
+
+execute 'httpd_firewall' do
+  command '/usr/bin/firewall-cmd  --permanent --zone public --add-service http'
+  ignore_failure true
+end
+
+execute 'reload_firewall' do
+  command '/usr/bin/firewall-cmd --reload'
+  ignore_failure true
+end
